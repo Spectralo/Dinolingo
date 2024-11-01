8a8db51764e26d16db7827790687e5b65dfe0136
spectralo <arthur.hardy@mageekbox.net>
2024-11-01 19:38:52 +0100

    Cleaned repo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,31 +5,17 @@ import {
   adaptNavigationTheme,
 } from "react-native-paper";
 
-import {
-  NavigationContainer,
-  DarkTheme as NavigationDarkTheme,
-} from "@react-navigation/native";
-
 // Navigation bar transparent
 import * as NavigationBar from "expo-navigation-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 NavigationBar.setPositionAsync("absolute");
 NavigationBar.setBackgroundColorAsync("#ffffff01");
-
-const { DarkTheme } = adaptNavigationTheme({
-  reactNavigationDark: NavigationDarkTheme,
-});
 
 export default function RootLayout() {
   // Dark or Light mode
   return (
     <PaperProvider>
-      <NavigationContainer independent={true} theme={DarkTheme}>
-        <SafeAreaProvider style={{ flex: 1 }}>
-          <Slot />
-        </SafeAreaProvider>
-      </NavigationContainer>
+      <Slot />
     </PaperProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,29 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import WelcomeScreen from "./welcome";
-import LoginScreen from "./login";
+import * as Linking from "expo-linking";
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+} from "@react-navigation/native";
+import { adaptNavigationTheme } from "react-native-paper";
+
+const prefix = Linking.createURL("/");
+
+const { DarkTheme } = adaptNavigationTheme({
+  reactNavigationDark: NavigationDarkTheme,
+});
 
 export default function App() {
   const Stack = createStackNavigator();
+  const linking = {
+    prefixes: [prefix],
+  };
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    </Stack.Navigator>
+    <NavigationContainer independent={true} linking={linking} theme={DarkTheme}>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
